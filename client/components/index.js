2bc7b210f9845676dfa7561d764aaa0693d9896e
Vinnie Magro <vmagro@fb.com>
2017-07-07 21:10:38 -0700

    Render list of persons

--- client/components/index.js
+++ client/components/index.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 import { gql, graphql } from 'react-apollo'
 
-function IndexComponent(props) {
+function IndexComponent({data: {persons}}) {
+  persons = persons || [];
   return (
     <div>
-      <ul></ul>
+      <ul>
+        {persons.map((p) => <li key={p.id}>{p.id} - {p.name}</li>)}
+      </ul>
       Hello world!
     </div>
   );
