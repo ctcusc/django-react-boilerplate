3cff06edc0428b1dc34452f5e4698ccdfb643069
Vinnie Magro <v@vinnie.io>
2017-08-27 11:21:30 -0700

    Remove intentionally failing test

--- server/api/schema_tests.py
+++ server/api/schema_tests.py
@@ -1,8 +1,3 @@
 def test_sanity():
     fish = 4
-    assert 2 + 2 == fish
-
-
-def test_fail():
-    fish = 'fish'
-    assert 2 + 2 == fish, 'FAIRY GODPARENTS!!!!'
+    assert 2 + 2 == fish, 'FAIRY GODPARENTS!!!'
