31a4465a31209e745a0a875fa92ca45810088ee0
Vinnie Magro <v@vinnie.io>
2017-09-11 16:17:57 -0700

    Remove graphql from urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,4 +1,4 @@
-"""server URL Configuration
+"""server URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -12,39 +12,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
 """
+
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
 
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-from rest_framework.schemas import get_schema_view
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'url', 'username', 'email', 'is_staff')
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-schema_view = get_schema_view(title='django-react-starter')
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^schema/$', schema_view),
     url(r'^dev-api/', include('rest_framework.urls')),
-    url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     url(r'^admin/', admin.site.urls),
 ]
