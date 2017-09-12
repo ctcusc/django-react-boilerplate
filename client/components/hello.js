d2f83fcff1ecbcd68f15b67f36796732107fe050
Vinnie Magro <v@vinnie.io>
2017-09-11 18:23:37 -0700

    Nuke client/ to merge in Alex's work

--- client/components/hello.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-class IndexComponent extends Component {
-  render() {
-    return <div>Hello world! (hello component)</div>
-  }
-}
-
-export default IndexComponent
--- client/components/hero.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-class HeroComponent extends Component {
-  render() {
-    return <div>Hello world!</div>
-  }
-}
-
-export default IndexComponent
