5fab87ed3808feb404f64dde53d7af7c8675bb9a
alexander-lee <thealexanderlee@me.com>
2017-09-17 17:13:43 -0700

    newer install script

--- install.sh
+++ install.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
 
-set -e;
+if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+  PROFILE="$HOME/.zsh_profile"
+elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+  PROFILE="$HOME/.bash_profile"
+fi
 
-brew install supervisord nginx python3 node yarn
+brew install nginx python3 yarn
 
+# ============= INSTALL NVM AND NODE ============= #
+# NOTE: `brew uninstall node` if you already have it installed
+brew install nvm
+echo '' >> $PROFILE
+echo 'export NVM_DIR="$HOME/.nvm"' >> $PROFILE
+echo '. "$(brew --prefix nvm)/nvm.sh"' >> $PROFILE
+
+source $PROFILE
+nvm install lts/*
+nvm alias default lts/*
+
+# ============= INSTALL SUPERVISOR ============= #
+brew install python
+if hash pip 2>/dev/null
+then
+  echo "pip already installed!"
+else
+  sudo easy_install pip
+fi
+if supervisord --version | grep 3.3.3 >/dev/null 2>&1
+then
+echo supervisord 3.3.3 already installed
+else
+sudo pip install supervisor
+fi
+
+# ============= SETUP SERVER ============= #
 # setup server
 pushd server/
 
