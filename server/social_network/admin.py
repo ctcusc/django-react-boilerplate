b430783d2dae314d1fd4fa3243b0119339479ba2
Vinnie Magro <v@vinnie.io>
2017-09-19 22:47:58 -0700

    Add models to admin site
    We want to add our new models to the Django admin site so that we can
    start to create, edit, and delete models before we finish implementing
    the rest of the our API.
    Once you add these lines, go to the admin interface and see that they
    were added! http://localhost:8000/admin

--- server/social_network/admin.py
+++ server/social_network/admin.py
@@ -1,3 +1,10 @@
 """Admin-site customization for social_network."""
 
 from django.contrib import admin
+
+from .models import Profile, Post, Vote
+# We can register models here so that they can be edited in the Django admin site
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Vote)
