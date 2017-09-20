6f2159b0fa8bea5ace166dfeede13ec7ee7035df
Vinnie Magro <v@vinnie.io>
2017-09-19 22:57:13 -0700

    Create PostSerializer
    Create a Django REST Framework Serializer class for Post objects.
    This allows us to create/read/update/list/delete Posts in our database.
    
    At this point we don't have a simple way to actually run this code, but
    in the next task we'll add API endpoints to actually use our
    PostSerializer class

--- server/social_network/serializers.py
+++ server/social_network/serializers.py
@@ -5,3 +5,23 @@ For more information, see http://www.django-rest-framework.org/tutorial/1-serial
 """
 
 from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.Serializer):
+    """Serialize a Post to/from the database."""
+
+    title = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        """Create and return a new `Post` instance, given the validated data."""
+        return Post.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return an existing `Post` instance, given the validated data."""
+        instance.title = validated_data.get('title', instance.title)
+        instance.created = validated_data.get('created', instance.style)
+        instance.save()
+        return instance
