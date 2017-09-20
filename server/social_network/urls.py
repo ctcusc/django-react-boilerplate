aabffa9d6d0fbe351d49b2bad138f15545012b2f
Vinnie Magro <v@vinnie.io>
2017-09-19 23:01:44 -0700

    Create views for posts
    In the last task, we added a serializer to convert Posts to and from
    our API representations.
    
    After adding the code in this task, use your favorite REST interface
    (such as
    Postman) to try some requests on http://localhost:8000/api/posts - at
    this point we've implemented support for listing all posts (GET to
    /api/posts), creating a post (POST to /api/posts), getting an
    individual post by ID (GET to /api/posts/<id>), updating a post (PUT to
    /api/posts/<id>) and deleting a post (DELETE to /api/posts/<id>)
    
    As you might have noticed, that's a lot of code to do not a whole lot.
    Fortunately Django REST Framework provides a lot of conveniences for us
    that we'll explore in later tasks.

--- server/social_network/urls.py
+++ server/social_network/urls.py
@@ -2,5 +2,9 @@
 
 from django.conf.urls import include, url
 
+from . import views
+
 urlpatterns = [
+    url(r'^posts/?$', views.post_list),
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
--- server/social_network/views.py
+++ server/social_network/views.py
@@ -1,1 +1,51 @@
 """API views for social_network."""
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+# we're simplifying things by using csrf_exempt, in a real application we would want the security
+# that it provides
+@csrf_exempt
+def post_list(request):
+    """List all posts, or create a new post."""
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def post_detail(request, pk):
+    """Retrieve, update or delete an individual Post."""
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
