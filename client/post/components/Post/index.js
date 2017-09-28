beee9d9fc36a15a44fbc639506db8d59ddc74c11
alexander-lee <thealexanderlee@me.com>
2017-09-27 20:58:27 -0700

    initial post

--- client/post/components/Post/index.js
+++ client/post/components/Post/index.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+class Post extends React.Component {
+  // PropTypes
+  static propTypes = {
+    title: PropTypes.string,
+  };
+  // State
+  state = {
+    votes: 0,
+    test: 1,
+  };
+
+  onUpvote() {
+    this.setState({
+      votes: this.state.votes + 1,
+    });
+  }
+  // Render
+  render() {
+    return (
+      <div>
+        <button onClick={this.onUpvote}>^</button>
+        <div>{this.state.votes}</div>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+
+export default Post;
